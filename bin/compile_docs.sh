f818a23b4863ff0d3c312ce489a5a17f8008d6a1
Toby Scales <17342265+tobyscales@users.noreply.github.com>
2023-10-13 15:53:12 -0600

    Update compile_docs.sh

--- bin/compile_docs.sh
+++ bin/compile_docs.sh
@@ -1,65 +1,65 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-echo "git branch -D apiBotTempBranch"
-git branch -D apiBotTempBranch
+#echo "git branch -D apiBotTempBranch"
+#git branch -D apiBotTempBranch
 
-echo "git checkout -b apiBotTempBranch"
-git checkout -b apiBotTempBranch
+#echo "git checkout -b apiBotTempBranch"
+#git checkout -b apiBotTempBranch
 
-echo "git branch -D $BRANCH"
-git branch -D $BRANCH
+#echo "git branch -D $BRANCH"
+#git branch -D $BRANCH
 
-echo "git checkout $BRANCH"
-git checkout $BRANCH
+#echo "git checkout $BRANCH"
+#git checkout $BRANCH
 
-echo "git branch -D apiBotTempBranch"
-git branch -D apiBotTempBranch
+#echo "git branch -D apiBotTempBranch"
+#git branch -D apiBotTempBranch
 
-if [ -d $BRANCH ]
-  then
-    echo "Directory exists, do nothing"
-  else
-    mkdir $BRANCH
-fi
+#if [ -d $BRANCH ]
+#  then
+#    echo "Directory exists, do nothing"
+#  else
+#    mkdir $BRANCH
+#fi
 
-echo "node blueprint-docify/index.js $BRANCH"
-node blueprint-docify/index.js $BRANCH
+echo "node bin/index.js $BRANCH"
+node bin/index.js $BRANCH
 
-echo "git add $BRANCH"
-git add $BRANCH
+#echo "git add $BRANCH"
+#git add $BRANCH
 
-echo "git stash"
-git stash
+#echo "git stash"
+#git stash
 
-echo "git branch -D gh-pages"
-git branch -D gh-pages
+#echo "git branch -D gh-pages"
+#git branch -D gh-pages
 
-echo "git checkout gh-pages"
-git checkout gh-pages
+#echo "git checkout gh-pages"
+#git checkout gh-pages
 
-if [ -d $BRANCH ]
-  then
-    echo "rm -rf -- $BRANCH"
-    rm -rf -- $BRANCH
+#if [ -d $BRANCH ]
+#  then
+#    echo "rm -rf -- $BRANCH"
+#    rm -rf -- $BRANCH
 
-    echo "add $BRANCH"
-    git add $BRANCH -u
-  else
-    echo "No directory, delete nothing"
-fi
+#    echo "add $BRANCH"
+#    git add $BRANCH -u
+#  else
+#    echo "No directory, delete nothing"
+#fi
 
-echo "git stash apply"
-git stash apply
+#echo "git stash apply"
+#git stash apply
 
 echo "git status"
 git status
 
-echo "git add $BRANCH"
-git add $BRANCH
+#echo "git add $BRANCH"
+#git add $BRANCH
 
-echo "git commit -m "Update API documentation [skip ci][no-ci]""
-git commit -m "Update API documentation [skip ci][no-ci]"
+#echo "git commit -m "Update API documentation [skip ci][no-ci]""
+#git commit -m "Update API documentation [skip ci][no-ci]"
 
-echo "git push tobyscales@github.com:tobyscales/openmetadataio-apidef.git gh-pages"
-git push tobyscales@github.com:tobyscales/openmetadataio-apidef.git gh-pages
+#echo "git push tobyscales@github.com:tobyscales/openmetadataio-apidef.git gh-pages"
+#git push tobyscales@github.com:tobyscales/openmetadataio-apidef.git gh-pages
